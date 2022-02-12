ec8bc70a2bc3ec9de393f54f509b9d628a1c5d9f
Daniel Gruber <daniel.gruber@espero.tech>
2022-02-12 18:52:28 +0100

    - Add spring

---
+++ src/main/kotlin/tech/espero/gruber/fullstackchallenge/FullstackchallengeApplication.kt
@@ -0,0 +1,11 @@
+package tech.espero.gruber.fullstackchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FullstackchallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FullstackchallengeApplication>(*args)
+}
---
+++ src/test/kotlin/tech/espero/gruber/fullstackchallenge/FullstackchallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.espero.gruber.fullstackchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FullstackchallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
