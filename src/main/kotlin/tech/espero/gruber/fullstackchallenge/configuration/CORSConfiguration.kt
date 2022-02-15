54c48cace6768ee5cb64ac88f47bede476499ba3
Daniel Gruber <daniel.gruber@espero.tech>
2022-02-15 17:13:30 +0100

    - more CORS

---
+++ src/main/kotlin/tech/espero/gruber/fullstackchallenge/configuration/CORSConfiguration.kt
@@ -0,0 +1,20 @@
+package tech.espero.gruber.fullstackchallenge.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CORSConfiguration: WebMvcConfigurer {
+    /**
+     * Configures CORS allowance.
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedHeaders("*")
+            .allowedMethods("*")
+            .allowedOrigins("http://localhost:3000")
+            .allowCredentials(true)
+    }
+}
